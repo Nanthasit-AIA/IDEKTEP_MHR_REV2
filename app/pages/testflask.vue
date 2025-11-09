e511af7cb526b19dc083703343bd17ff86929b8c
Nanthasit-AIA <127289457+Nanthasit-AIA@users.noreply.github.com>
2025-11-09 16:32:11 +0700

    second commit

---
+++ app/pages/testflask.vue
@@ -0,0 +1,11 @@
+<script setup>
+const { data, pending, error } = await useFetch('http://localhost:5000/api/hello')
+</script>
+
+<template>
+  <div>
+    <p v-if="pending">Loading...</p>
+    <p v-else-if="error">Error loading API</p>
+    <p v-else>{{ data.message }}</p>
+  </div>
+</template>
